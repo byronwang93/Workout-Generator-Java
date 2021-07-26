a46605d29ea22b2ee3ec09d9b3955a764fcaba5e
byronwang93 <byronwang123919@gmail.com>
2021-07-25 20:14:33 -0700

    updated to include the other user stories

--- src/main/ui/WorkoutGenerator.java
+++ src/main/ui/WorkoutGenerator.java
@@ -1,5 +1,8 @@
 package ui;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
 import model.Activity;
@@ -33,8 +36,111 @@ public class WorkoutGenerator {
     }
 
     //MODIFIES: this
-    //EFFECTS: processes the user's input
+    //EFFECTS: processes the user's input of choice
     private void processCommand(String command) {
+        if (command.equals("1")) {
+            displayIntensityOptions();
+            command = input.next();
+            command = command.toLowerCase();
+            processIntensityCommand(command);
+        } else if (command.equals("2")) {
+            addUserExercise();
+        } else if (command.equals("3")) {
+            removeUserExercise();
+        } else if (command.equals("4")) {
+            changeIntensity();
+        } else if (command.equals("5")) {
+            showOptions();
+        } else if (command.equals("6")) {
+            System.exit(0);
+        } else {
+            System.out.println("Input not valid.");
+        }
+    }
+
+    //MODIFIES: this
+    //EFFECTS: shows user all the exercise option descriptions in generator
+    private void showOptions() {
+
+        List<String> finalWorkoutDesc = new ArrayList<>();
+
+        for (Activity act : workout.getOptions()) {
+            finalWorkoutDesc.add(act.getDescription());
+        }
+
+        System.out.println("All option descriptions:");
+
+        for (String name : finalWorkoutDesc) {
+            System.out.println(name);
+        }
+    }
+
+    //MODIFIES: this
+    //EFFECTS: changes the intensity of the user's desired workout
+    private void changeIntensity() {
+        String intensity;
+        String desc;
+        Scanner input2 = new Scanner(System.in);
+        String command2 = null;
+        System.out.println("Description of activity you want to change");
+        command2 = input2.next();
+        desc = command2;
+        Scanner input = new Scanner(System.in);
+        String command = null;
+        System.out.println("What intensity do you want to change your exercise option to? (easy, medium, or hard)");
+        command = input.next();
+        intensity = command;
+
+        for (Activity act : workout.getFinalWorkout()) {
+            if (act.getDescription().equals(desc)) {
+                act.changeIntensity(intensity);
+            } else {
+                System.out.printf("That exercise doesn't exist");
+            }
+        }
+    }
+
+    //MODIFIES: this
+    //EFFECTS: adds a user's input into the workout options
+    private void addUserExercise() {
+        String desc;
+        String intensity;
+        String muscGroup;
+        String equip;
+        Scanner input = new Scanner(System.in);
+        String command = null;
+        System.out.println("Put its description here! (ex. 5 x 20 pushups)");
+        command = input.next();
+        desc = command;
+        System.out.println("Intensity of workout! (easy, medium or hard)");
+        command = input.next();
+        intensity = command;
+        System.out.println("Muscle group? (arms, legs, or core)");
+        command = input.next();
+        muscGroup = command;
+        System.out.println("Equipment needed? (true or false)");
+        command = input.next();
+        equip = command;
+        Activity newAct = new Activity(desc, intensity, muscGroup, Boolean.parseBoolean(equip));
+        workout.addActivity(newAct);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: adds a user's input into the workout options
+    private void removeUserExercise() {
+        String desc;
+        Scanner input = new Scanner(System.in);
+        String command = null;
+        System.out.println("Description of workout you want removed.");
+        command = input.next();
+        desc = command;
+
+        final boolean b = workout.getFinalWorkout().removeIf(act -> act.getDescription().equals(desc));
+    }
+
+    //MODIFIES: this
+    //EFFECTS: processes the user's input for intensity
+    private void processIntensityCommand(String command) {
         if (command.equals("easy")) {
             workout.filterIntensity("easy");
             processMuscGroupAndEquipment();
@@ -47,7 +153,6 @@ public class WorkoutGenerator {
         } else {
             System.out.println("Intensity is not valid.");
         }
-
     }
 
     //MODIFIES: this
@@ -57,12 +162,23 @@ public class WorkoutGenerator {
         input = new Scanner(System.in);
     }
 
-    //EFFECTS: displays a menu of intensities to the user
+    //EFFECTS: displays a main menu that allows user to add, remove or generate a workout
     private void displayOptions() {
-        System.out.println("\nSelect intensity level:");
-        System.out.println("\neasy");
-        System.out.println("\nmedium");
-        System.out.println("\nhard");
+        System.out.println("Welcome! Choose an option below!");
+        System.out.println("- Generate Workout (enter 1)");
+        System.out.println("- Add an activity (enter 2)");
+        System.out.println("- Remove an activity (enter 3)");
+        System.out.println("- Change the intensity of an existing option (enter 4)");
+        System.out.println("- View the possible workout options (enter 5)");
+        System.out.println("- Quit (enter 6)");
+    }
+
+    //EFFECTS: displays a menu of intensities to the user
+    private void displayIntensityOptions() {
+        System.out.println("Select intensity level:");
+        System.out.println("easy");
+        System.out.println("medium");
+        System.out.println("hard");
     }
 
     //MODIFIES: this
